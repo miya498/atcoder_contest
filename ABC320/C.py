0e922c4f411d519138cc10f8a78c4c7863862dad
miya498 <ugimaru262@gmail.com>
2024-11-03 14:51:33 +0900

    add ABC320

---
+++ ABC320/C.py
@@ -0,0 +1,15 @@
+M = int(input())
+N = 3
+S = [input() for _ in range(N)]
+INF = 1e9
+ans = INF
+for i in range(N * M):
+    for j in range(N * M):
+        for k in range(N * M):
+            if i != j and i != k and j != k and S[0][i % M] == S[1][j % M] == S[2][k % M]:
+                ans = min(ans, max(i, j, k))
+
+if ans == INF:
+    print(-1)
+else:
+    print(ans)
