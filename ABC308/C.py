e27aec181247be396538008e7a212248a2a9da9e
miya498 <ugimaru262@gmail.com>
2024-11-14 10:49:01 +0900

    fix ABC308 C

--- ABC308/C.py
+++ ABC308/C.py
@@ -3,10 +3,10 @@ A_B = []
 for i in range(N):
     a,b = map(int,input().split())
     if b != 0:
-        dev = a/b
+        dev = a/b*(10**100)
         A_B.append([dev,i+1])
     else:
-        dev = 1
+        dev = 10**100
         A_B.append([dev,i+1])
 
 A_B.sort(key=lambda x: (-x[0],x[1]))
