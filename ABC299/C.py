0feb6c16d48cc2bc0fbd8cda4d36cfc42a9b420c
DESKTOP-OJF0LRG\miya <20rd170@ms.dendai.ac.jp>
2024-11-24 18:17:31 +0900

    fix ABC299 C

--- ABC299/C.py
+++ ABC299/C.py
@@ -1,2 +1,11 @@
 N = int(input())
 S = input()
+
+if '-' not in S or 'o' not in S:
+    print(-1)
+    exit()
+
+dango = S.split("-")
+
+ans = max(len(segment) for segment in dango if segment)
+print(ans)
