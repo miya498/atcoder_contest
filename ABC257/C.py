30ecb5441dfbbe2a84e65ae38554360056f83253
DESKTOP-OJF0LRG\miya <ugimaru262@gmail.com>
2025-01-11 17:54:29 +0900

    fix ABC257 C

--- ABC257/C.py
+++ ABC257/C.py
@@ -5,26 +5,19 @@ W = list(map(int, input().split()))
 # 体重の情報を (体重, 本来の属性) のペアにしてリスト化
 people = [(W[i], int(S[i])) for i in range(N)]
 
-# 体重でソート
-people.sort()
+people.sort(key=lambda x: (x[0],x[1]))
 
-# 初期値として、すべての人を子供と判定した場合の正答数
-correct = sum(1 for _, attr in people if attr == 0)
+correct = sum(1 for _, attr in people if attr == 1)
 max_correct = correct
 
 # 境界値ごとに正答数を計算
 for i in range(N):
     if people[i][1] == 0:
-        correct -= 1  # 本来の子供を大人と判定する
+        correct += 1
     else:
-        correct += 1  # 本来の大人を大人と判定する
+        correct -= 1
+    
+    if i == N-1 or people[i][0] != people[i+1][0]:
+        max_correct = max(correct,max_correct)
 
-    # 次の体重が同じならスキップ
-    if i < N - 1 and people[i][0] == people[i + 1][0]:
-        continue
-
-    # 正答数を更新
-    max_correct = max(max_correct, correct)
-
-# 結果を出力
 print(max_correct)
