d7e93638a4a3e4c21b4a04aefc45130a1bd6c096
DESKTOP-OJF0LRG\miya <ugimaru262@gmail.com>
2025-01-11 18:06:38 +0900

    fix ABC332 C

--- ABC331~340/ABC332/C.py
+++ ABC331~340/ABC332/C.py
@@ -6,11 +6,26 @@ mugi_siyou = 0
 logo = 0
 logo_siyou = 0
 for i in range(N):
-    if S[i] == 0:
+    if S[i] == "0":
         mugi += mugi_siyou
         logo += logo_siyou
         mugi_siyou = 0
         logo_siyou = 0
-    elif S[i] == 1:
+    elif S[i] == "1":
         if mugi == 0:
-            
+            if logo == 0:
+                logo_siyou += 1
+            else:
+                logo -= 1
+                logo_siyou += 1
+        else:
+            mugi -= 1
+            mugi_siyou += 1
+    else:
+        if logo == 0:
+            logo_siyou += 1
+        else:
+            logo -= 1
+            logo_siyou += 1
+
+print(logo+logo_siyou)
