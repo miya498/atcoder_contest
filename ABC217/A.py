64390576e655e4678577436076d94fc5fb374cd2
DESKTOP-OJF0LRG\miya <ugimaru262@gmail.com>
2025-02-16 15:53:51 +0900

    add ABC217

---
+++ ABC217/A.py
@@ -0,0 +1,8 @@
+S,T = input().split()
+dic = [S,T]
+dic.sort()
+
+if dic[0] == S:
+    print("Yes")
+else:
+    print("No")
---
+++ ABC217/B.py
@@ -0,0 +1,11 @@
+S_1 = input()
+S_2 = input()
+S_3 = input()
+
+atcoder = ["ABC" , "ARC" , "AGC" , "AHC"]
+
+atcoder.remove(S_1)
+atcoder.remove(S_2)
+atcoder.remove(S_3)
+
+print(atcoder[0])
