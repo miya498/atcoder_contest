242f508481324d41d7b8711622249f023655250d
DESKTOP-OJF0LRG\miya <ugimaru262@gmail.com>
2025-03-07 11:50:02 +0900

    fix ABC199

--- ABC199/C.py
+++ ABC199/C.py
@@ -2,13 +2,20 @@ N = int(input())
 S = list(input())
 Q = int(input())
 queries = [list(map(int,input().split())) for _ in range(Q)]
-
+flag = False
 for q in queries:
     if q[0] == 1:
         a,b = q[1]-1,q[2]-1
+        if flag:
+            a = a+N
+            b = b-N
         S[a],S[b] = S[b],S[a]
     elif q[0] == 2:
-        S = S[N:]+S[:N]
+        if flag:
+            flag = False
+        else:
+            flag = True
 
+if flag:
+    S = S[N:]+S[:N]
 print("".join(S))
-
