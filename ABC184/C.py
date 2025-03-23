ff9438cd2e9b46389503e31fffe50582eaac1595
DESKTOP-OJF0LRG\miya <ugimaru262@gmail.com>
2025-03-23 23:49:29 +0900

    fix ABC184 C

--- ABC184/C.py
+++ ABC184/C.py
@@ -1,21 +1,14 @@
 r1, c1 = map(int, input().split())
 r2, c2 = map(int, input().split())
-r = r2 - r1
-c = c2 - c1
 
-if (r, c) == (0, 0):
-    ans = 0
-elif r == c or r == -c: 
-    ans = 1
-elif abs(r) + abs(c) <= 3:
-    ans = 1
-elif (r ^ c ^ 1) & 1:
-    ans = 2
-elif abs(r) + abs(c) <= 6:
-    ans = 2
-elif abs(r + c) <= 3 or abs(r - c) <= 3:
-    ans = 2
+dx = abs(r1 - r2)
+dy = abs(c1 - c2)
+
+if r1 == r2 and c1 == c2:
+    print(0)
+elif dx + dy <= 3 or r1 + c1 == r2 + c2 or r1 - c1 == r2 - c2:
+    print(1)
+elif dx + dy <= 6 or abs(dx - dy) <= 3 or (dx + dy) % 2 == 0:
+    print(2)
 else:
-    ans = 3
-
-print(ans)
+    print(3)
